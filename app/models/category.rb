8614030cdbbd59b2624f442e9cdd4979a74d8199
Louise Stone <louisemstone@hotmail.com>
2024-09-30 10:39:26 +0100

    associations added

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,2 +1,4 @@
 class Category < ApplicationRecord
+  has_many :bookmarks, dependent: :destroy
+  has_many :recipes, through: :bookmarks
 end
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,2 +1,4 @@
 class Recipe < ApplicationRecord
+  has_many :bookmarks
+  has_many :categories, through: :bookmarks
 end
